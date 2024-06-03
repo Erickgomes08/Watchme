122d19cc2b665e5b944626517462d5a4a6e3e4ca
erickgomes <erick.gomes.fulltime@gmail.com>
2024-06-03 11:04:24 -0300

    organizando códigos e imagens

--- js/slide.js
+++ js/slide.js
@@ -32,6 +32,5 @@ function showSlides() {
     }
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 9000);  
+    slides[slideIndex - 1].style.display = "block"; 
 }
