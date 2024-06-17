2548a8f18ff1495ab19eb9a83cf48b36f6a561a5
erickgomes <erick.gomes.fulltime@gmail.com>
2024-06-17 14:49:52 -0300

    changes

--- js/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector("button").addEventListener("click", function () {
-    var email = document.querySelector("input[type=text]").value;
-    alert("te enviaremos para outra pagina para continuar o processo: " + email);
-});
